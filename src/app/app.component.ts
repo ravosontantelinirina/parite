b0bd6295ed598e6536c5d4488184a1c0ae188a01
ravosontantelinirina <ravosontantelinirina7@gmail.com>
2023-10-30 01:23:25 +0300

    premier version de APk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TensorFlowV1';
+
+  nombreResultat!: number;
+  parite!: string ;
+  nombreEnBinaire! : string;
+  masque: number = 0b1111;
+  
+
+  convertir(nombre:number){
+      let teste = nombre & this.masque;
+      this.nombreEnBinaire = teste.toString(2).padStart(4,'0');
+      this.parite = "impaire";
+      if(parseInt(this.nombreEnBinaire.replace(/\D/g, '')[3]) == 0){
+        this.parite = "paire";
+      }
+  }
+ 
 }
